cfb67b2b7d730e28e8cde38937b3c61ce1d7ea09
Bharath <bharathsurya021@gmail.com>
2022-08-17 12:32:43 +0530

    product listing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import { Container } from 'react-bootstrap'
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import HomeScreen from "./screens/HomeScreen";
 
 
 function App() {
@@ -10,7 +11,7 @@ function App() {
       <main className='py-3'>
         <Container>
 
-          <h1>Welcome to e-shop</h1>
+          <HomeScreen />
         </Container>
       </main>
       <Footer />
