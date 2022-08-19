e41b38e2ce849cf3e755650ebfbf2869e131fcc3
Bharath <bharathsurya021@gmail.com>
2022-08-19 10:44:02 +0530

    add product routes to fetch from db

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,29 @@
+import express, { Router } from "express";
+import asyncHandler from 'express-async-handler'
+const router = express.Router()
+
+import Product from '../models/productModel.js'
+
+
+//fetch all products
+// Get /api/products
+// Public route
+router.get('/', asyncHandler(async (req, res) => {
+    const products = await Product.find({})
+    res.json(products)
+}))
+
+//fetch single product
+// Get /api/products/:id
+// Public route
+router.get('/:id', asyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id)
+    if (product) {
+
+        res.json(product)
+    } else {
+        res.status(404).json({ message: "Product not found" })
+    }
+}))
+
+export default router
