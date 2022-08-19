579ee8671854a628edb66c042d201338308b939e
Bharath <bharathsurya021@gmail.com>
2022-08-19 11:47:48 +0530

    redux setup

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux'
+import store from './store';
 import './bootstrap.min.css'
 import './index.css';
 import App from './App';
@@ -7,11 +9,11 @@ import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <BrowserRouter>
 
       <App />
     </BrowserRouter>
-  </React.StrictMode>
+  </Provider>
 );
 
