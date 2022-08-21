9da8723bbe7fed40253c33ca2054c7befbe853dc
Bharath <bharathsurya021@gmail.com>
2022-08-21 23:01:51 +0530

    login screen fn

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import Footer from "./components/Footer";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from './screens/ProductScreen';
 import CartScreen from './screens/CartScreen';
+import LoginScreen from './screens/LoginScreen';
 
 
 function App() {
@@ -14,10 +15,11 @@ function App() {
       <main className='py-3'>
         <Container>
           <Routes>
-            <Route path='/' element={< HomeScreen />} />
+            <Route path='/login' element={< LoginScreen />} />
             <Route path='/product/:id' element={< ProductScreen />} />
             <Route path='/cart' element={< CartScreen />} />
             <Route path='/cart/:id' element={< CartScreen />} />
+            <Route path='/' element={< HomeScreen />} />
           </Routes>
         </Container>
 
