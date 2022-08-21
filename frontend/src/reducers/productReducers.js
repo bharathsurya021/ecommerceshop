28118235e5787e6470dc3eb1f95acce27a6fc97e
Bharath <bharathsurya021@gmail.com>
2022-08-21 06:02:32 +0530

    add product details in redux state

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -1,4 +1,4 @@
-import { PRODUCT_LIST_FAILURE, PRODUCT_LIST_SUCCESS, PRODUCT_LIST_REQUEST } from '../constants/productConstants'
+import { PRODUCT_LIST_FAILURE, PRODUCT_LIST_SUCCESS, PRODUCT_LIST_REQUEST, PRODUCT_DETAILS_REQUEST, PRODUCT_DETAILS_SUCCESS, PRODUCT_DETAILS_FAILURE } from '../constants/productConstants'
 
 
 
@@ -9,6 +9,20 @@ export const productListReducer = (state = { products: [] }, action) => {
         case PRODUCT_LIST_SUCCESS:
             return { loading: false, products: action.payload }
         case PRODUCT_LIST_FAILURE:
+            return { loading: false, error: action.payload }
+        default:
+            return state
+    }
+}
+
+
+export const productDetailsReducer = (state = { product: { reviews: [] } }, action) => {
+    switch (action.type) {
+        case PRODUCT_DETAILS_REQUEST:
+            return { loading: true, ...state }
+        case PRODUCT_DETAILS_SUCCESS:
+            return { loading: false, product: action.payload }
+        case PRODUCT_DETAILS_FAILURE:
             return { loading: false, error: action.payload }
         default:
             return state
