2ccc333055cd44336471ab564ca792db1ad6a429
Bharath <bharathsurya021@gmail.com>
2022-08-23 17:43:07 +0530

    getorder by id end point & order screen

---
+++ frontend/src/reducers/orderReducers.js
@@ -0,0 +1,48 @@
+import { ORDER_CREATE_REQUEST, ORDER_CREATE_SUCCEESS, ORDER_CREATE_FAILURE, ORDER_DETAILS_FAILURE, ORDER_DETAILS_SUCCEESS, ORDER_DETAILS_REQUEST } from '../constants/orderConstants'
+
+export const orderCreateReducer = (state = {}, action) => {
+    switch (action.type) {
+        case ORDER_CREATE_REQUEST:
+            return {
+                loading: true
+            }
+        case ORDER_CREATE_SUCCEESS:
+            return {
+                loading: false,
+                success: true,
+                order: action.payload
+            }
+        case ORDER_CREATE_FAILURE:
+            return {
+                loading: false,
+                error: action.payload
+            }
+        default:
+            return state
+    }
+}
+
+export const orderDetailsReducer = (
+    state = { loading: true, orderItems: [], shippingAddress: {} },
+    action
+) => {
+    switch (action.type) {
+        case ORDER_DETAILS_REQUEST:
+            return {
+                ...state,
+                loading: true,
+            }
+        case ORDER_DETAILS_SUCCEESS:
+            return {
+                loading: false,
+                order: action.payload,
+            }
+        case ORDER_DETAILS_FAILURE:
+            return {
+                loading: false,
+                error: action.payload,
+            }
+        default:
+            return state
+    }
+}
