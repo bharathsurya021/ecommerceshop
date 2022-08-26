ce0fbe49b981ececd4394d99448a66fa62560d2b
Bharath <bharathsurya021@gmail.com>
2022-08-26 11:16:00 +0530

    frontend admin userlist access

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,6 +12,7 @@ import ShippingScreen from './screens/ShippingScreen';
 import PaymentScreen from './screens/PaymentScreen';
 import PlaceOrderScreen from './screens/PlaceOrderScreen';
 import OrderScreen from './screens/OrderScreen';
+import UserListScreen from './screens/UserListScreen';
 
 
 function App() {
@@ -21,6 +22,7 @@ function App() {
       <main className='py-3'>
         <Container>
           <Routes>
+
             <Route path='/order/:id' element={< OrderScreen />} />
             <Route path='/placeorder' element={< PlaceOrderScreen />} />
             <Route path='/payment' element={< PaymentScreen />} />
@@ -32,6 +34,7 @@ function App() {
             <Route path='/product/:id' element={< ProductScreen />} />
             <Route path='/cart' element={< CartScreen />} />
             <Route path='/cart/:id' element={< CartScreen />} />
+            <Route path='/admin/userlist' element={< UserListScreen />} />
             <Route path='/' element={< HomeScreen />} />
           </Routes>
         </Container>
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -3,7 +3,7 @@ import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import { productListReducer, productDetailsReducer } from './reducers/productReducers'
 import { cartReducer } from './reducers/cartReducers'
-import { userLoginReducer, userRegisterReducer, userDetailsReducer, userUpdateProfileReducer } from './reducers/userReducers'
+import { userLoginReducer, userRegisterReducer, userDetailsReducer, userUpdateProfileReducer, userListReducer } from './reducers/userReducers'
 import { orderCreateReducer, orderDetailsReducer, orderListMyReducer, orderPayReducer } from './reducers/orderReducers'
 
 const reducer = combineReducers({
@@ -18,6 +18,7 @@ const reducer = combineReducers({
     orderDetails: orderDetailsReducer,
     orderPay: orderPayReducer,
     myOrderList: orderListMyReducer,
+    userList: userListReducer,
 })
 
 const cartItemsFromStorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : []
